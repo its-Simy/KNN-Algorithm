5598a0764a218447e93513e4d625740bbb33a8e6
its-Simy <135281067+its-Simy@users.noreply.github.com>
2025-02-09 22:15:14 -0500

    Some changes
    Still needs fixing but there is progress in the file reading.

--- KNN.py
+++ KNN.py
@@ -1,7 +1,6 @@
 import math
-#import matplotlib.pyplot as plt
+import csv
 
-#from sklearn.neighbors import KNeighborsClassifier
 from collections import Counter
 
 
@@ -38,7 +37,6 @@ class KNN:
         for test_point in x_test:
             distances = []
             for i, train_point in enumerate(self.training_data):
-                #dist = self.euclidean_distance(test_point,train_point)
                 dist = self.manhattan_distance(test_point, train_point)
                 
                 distances.append((dist,self.traning_label[i]))
@@ -55,12 +53,25 @@ class KNN:
             predictions.append(most_common_label)
         return predictions
     
+    def load_data(filename):
+       mylist = []
+       with open(filename) as numbers:
+           numbers_data = csv.reader(numbers, delimiter = ',')
+           for row in numbers_data:
+               mylist.append(row[0])
+           return mylist
+            
+        
+    
 if __name__ == "__main__":
     x_train = [[10,9],[1,4],[10,1],[9,1],[10,2],[7,3]]
     y_train = ['F','F','F','V','P','P']
     
-    file_reader = open("Data.txt", "r")
+    #file_reader = open("iris-dataset.csv", "r")
     
+    new_list = open('iris-dataset.csv','r')
+    for row in new_list:
+        print(row)
     
     
     x_test = [[3,5]]
@@ -70,10 +81,13 @@ if __name__ == "__main__":
     knn = KNN(1)
     knn.fit(x_test,y_train)
     prediction = knn.predict(x_test)
-    print(';Predictions: ', prediction)
+    #print(';Predictions: ', prediction)
    
-    str = file_reader.read()
-    #print(str)
+   # str = file_reader.read()
+   # print(str)
+    
     
     #The following is going to be the inputs and answer for the tester to see if classification is working
     #5.9,3,5.1,1.8,"Virginica"
+    
+    
