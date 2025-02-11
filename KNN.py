306918d914a844316ceb9fb9315613998dff4ebf
its-Simy <135281067+its-Simy@users.noreply.github.com>
2025-02-10 20:15:51 -0500

    Update to logic
    changes logic for some of the methods to adjust to data.

--- KNN.py
+++ KNN.py
@@ -8,7 +8,7 @@ class KNN:
     def __init__(self,k):
         self.K = k
         self.training_data = []
-        self.traning_label = []
+        self.training_label = []
         
     #this will take the input and just save it locally
     def fit(self, x, y):
@@ -32,7 +32,7 @@ class KNN:
             for i, train_point in enumerate(self.training_data):
                 dist = self.manhattan_distance(test_point, train_point)
                 
-                distances.append((dist,self.traning_label[i]))
+                distances.append((dist,self.training_label[i]))
             
             #tells the computer to sort by distance not by label    
             distances.sort(key = lambda x:x[0])
@@ -47,25 +47,34 @@ class KNN:
         return predictions
     
     
+    #grabs the data from the csv file, then divides it into 2 different lists, one with the data points, the other with the answer
+    #in the middle i need to manipulate the datapoints into floats to save them properly
     def manipulate_data(self, data):
         data_points = []
         classification = []
+        float_values = []
         
         divider = len(data[0])-1
     
         for row in data:
             classification.append(row[divider])
+            
+            for value in row[:divider]:
+                float_values.append(float(value))
+                
             data_points.append(row[:divider])
             
             
         return data_points, classification
     
+    
+    #open csv file, then appends everything except the header into a list to then call another method to manipulate the data provided
     def load_data(self, filename):
        mylist = []
        with open(filename) as numbers:
            numbers_data = csv.reader(numbers)
-           next(numbers_data)
            
+           next(numbers_data) #skips the header so it can start reading the data
            
            for row in numbers_data:
                mylist.append(row)
@@ -84,7 +93,7 @@ if __name__ == "__main__":
     knn1 = KNN(1)
     x_training_data, y_training_data = knn1.load_data('iris-dataset.csv')
     knn1.fit(x_training_data,y_training_data)
-    tester = [5.9,3,5.1,1.8]
+    tester = "5.9,3,5.1,1.8"
     answer = knn1.predict(tester)
     print(answer)
     
